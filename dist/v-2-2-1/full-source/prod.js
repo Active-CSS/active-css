f81325e08dfe2f24fa48103a843e3de20711d055
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2020-07-10 23:52:10 +0100

    v-2-2-1
    fix disappearing html variables that may appear inside the $STRING variable to be rendered as text.

--- dist/v-2-2-1/full-source/prod.js
+++ dist/v-2-2-1/full-source/prod.js
@@ -4585,29 +4585,24 @@ const _replaceScopedVarsExpr = (str, compRef=null) => {
 const _replaceStringVars = (o, str, compRef) => {
 	// This function should only deal once with {$STRING}, and once with HTML variables. Gets called for different reasons, hence it's purpose is double-up here.
 	// This is the function that translates HTML variables for an output string.
-	let noResYet = false;
+	let res = '';
 	str = str.replace(/\{([\u00BF-\u1FFF\u2C00-\uD7FF\w_\-\[\]\.\$]+)\}/gi, function(_, innards) {
 		if (innards == '$STRING') {
-			if (!o.res) {
-				noResYet = true;
-				return '{$STRING}';
+			if (o && o.res) {
+				res = o.res;
+			} else {
+				res = '{$STRING}';
 			}
-			return ((o && o.res) ? o.res : '');
+			return res;
 		} else if (innards.indexOf('$') !== -1) {
 			// This should be treated an HTML variable string. It's a regular Active CSS variable that allows HTML.
 			let scopedVar = ((compRef && privateScopes[compRef]) ? compRef : 'main') + '.' + innards;
-			let res = _get(scopedVars, scopedVar);
+			res = _get(scopedVars, scopedVar);
 			return res || '';
 		} else {
 			return '{' + innards + '}';
 		}
 	});
-	// We only allow html variables to be passed once - anything inside the result string that looks like a further HTML variable (contains a $) gets escaped.
-	if (!noResYet) {
-		str = str.replace(/\{([\u00BF-\u1FFF\u2C00-\uD7FF\w_\-\.\$]+)\}/gi, function(_, innards) {
-			return (innards.indexOf('$') !== -1) ? '' : '{' + innards + '}';
-		});
-	}
 	return str;
 };
 
@@ -4958,11 +4953,13 @@ const _ajaxCallbackDisplay = (o) => {
 	if (!o.error && o.preGet) {
 		// Store it for later.
 		ajaxResLocations[o.finalURL] = o.res;
+		delete o.res;
 	} else {
 		// Run the post event - success or failure.
 		_ajaxDisplay(o);
 		if (!o.error && o.cache) {
 			ajaxResLocations[o.finalURL] = o.res;
+			delete o.res;
 		}
 	}
 };
--- src/utils/file-handling/_ajaxCallbackDisplay.js
+++ src/utils/file-handling/_ajaxCallbackDisplay.js
@@ -2,11 +2,13 @@ const _ajaxCallbackDisplay = (o) => {
 	if (!o.error && o.preGet) {
 		// Store it for later.
 		ajaxResLocations[o.finalURL] = o.res;
+		delete o.res;
 	} else {
 		// Run the post event - success or failure.
 		_ajaxDisplay(o);
 		if (!o.error && o.cache) {
 			ajaxResLocations[o.finalURL] = o.res;
+			delete o.res;
 		}
 	}
 };
