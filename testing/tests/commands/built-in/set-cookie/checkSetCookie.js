12278b211676fc6739f0c6596e5bff5b135b2677
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2020-10-17 22:00:10 +0100

    Check-in prior to 2.4.0 release. Not recommended for use in production.
    JavaScript expression tests for expiry for set-cookie.

--- testing/tests/commands/built-in/set-cookie/checkSetCookie.js
+++ testing/tests/commands/built-in/set-cookie/checkSetCookie.js
@@ -7,6 +7,8 @@ function checkSetCookie(o) {
 	let test3 = _getCookie('test3');
 	let test4 = _getCookie('test4');
 	let test5 = _getCookie('test5');
+	let test6 = _getCookie('test6');
+	let test7 = _getCookie('test7');
 
 	if (test1 != 'Y') {
 		_fail(testEl, 'test1 cookie is not set to "Y"');
@@ -22,7 +24,12 @@ function checkSetCookie(o) {
 
 	if (test4 == 'expired%20cookie' || test5 != 'non-expired%20cookie') {
 		// Both these checks need to occur to ascertain whether a correctly formatted string date test works.
-		_fail(testEl, 'test4 cookie straight date test failed.');
+		_fail(testEl, 'Cookie straight date test failed.');
+	}
+
+	if (test6 == 'expression%20expired' || test7 != 'expression%20not%20expired') {
+		// Both these checks need to occur to ascertain whether an expression inserted as an expiry date with "{= =}" test works.
+		_fail(testEl, 'Cookie expression date test failed.');
 	}
 
 	_addSuccessClass(testEl);
