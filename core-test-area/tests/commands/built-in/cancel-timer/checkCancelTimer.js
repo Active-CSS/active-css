c4465ae910c974c0e49e08905c319b234c7c5467
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2020-10-19 15:19:16 +0100

    Remove unnecessary comment

--- core-test-area/tests/commands/built-in/cancel-timer/checkCancelTimer.js
+++ core-test-area/tests/commands/built-in/cancel-timer/checkCancelTimer.js
@@ -1,10 +1,3 @@
-/**
-*	The reason this directory has an underscore (_) is that it needs to run before all other tests. They run alphabetically according to directory name.
-*	Without the underscore, when the cancel-timer-all command runs it will cancel ALL commands that have a delay set which breaks any other tests that uses a delay
-*	So don't remove the underscore. This needs to go into a directory marked _cancel-timer-all, unless you want to move it into a different directory, which I
-*	wouldn't recommend as it takes it outside the test suites and will make it harder for people to find.
-*/
-
 function checkCancelTimerA(o) {
 	let testEl = _initTest('checkCancelTimer');
 	if (!testEl) return;
