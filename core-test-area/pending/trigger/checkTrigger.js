d3b052d9e88c885eba6d1bbcbe1b6048fb4dd1b6
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2020-11-08 20:35:18 +0000

    Check-in prior to 2.4.0 release. Not recommended for production use.
    Addition of trigger command test.

--- core-test-area/pending/trigger/checkTrigger.js
+++
@@ -1,8 +0,0 @@
-function checkTrigger(o) {
-	let testEl = _initTest('checkTrigger');
-	if (!testEl) return;
-
-//	_fail(testEl, 'message');
-
-	_addSuccessClass(testEl);
-}
--- core-test-area/startup/compiled/core-test-js.js
+++ core-test-area/startup/compiled/core-test-js.js
@@ -1607,6 +1607,20 @@ function checkToggleClassFinal(o) {
 	_addSuccessClass(testEl);
 }
 
+function checkTrigger(o) {
+	let testEl = _initTest('checkTrigger');
+	if (!testEl) return;
+
+	let el = _getObj('#checkTrigger p');
+	if (!el) {
+		_fail(testEl, '#checkTrigger p not there for the test.');
+	} else if (el.innerHTML !== 'Hello. Is it me you\'re looking for?') {
+		_fail(testEl, 'target div does not contain "Hello. Is it me you\'re looking for?" after test.');
+	} else {
+		_addSuccessClass(testEl);
+	}
+}
+
 function checkUrlChange(o) {
 	let testEl = _initTest('checkUrlChange');
 	if (!testEl) return;
