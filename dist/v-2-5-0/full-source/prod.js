9a26e6d7db0ab2453f88b28a1e1329241052af9c
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-02-02 19:25:37 +0000

    Commit compiled core for fix to clickoutside working in components.

--- dist/v-2-5-0/full-source/prod.js
+++ dist/v-2-5-0/full-source/prod.js
@@ -1756,19 +1756,13 @@ const _handleClickOutside = (el, e) => {
 	let cid, clickOutsideObj;
 	for (cid in clickOutsideSels) {
 		// Check the state of the clickoutside for this container. Will be true if active.
-
-console.log('_handleClickOutside, cid:', cid);
-
 		if (clickOutsideSels[cid][0]) {
 			// Does this clicked object exist in the clickoutside main element?
 			clickOutsideObj = idMap[cid];
 			if (clickOutsideObj && !clickOutsideObj.contains(el)) {
 				// This is outside.
-
-console.log('_handleClickOutside, is outside. clickOutsideObj:', clickOutsideObj);
 				// Get the component, scope, etc. for this element if there is component.
 				let compDetails = _componentDetails(clickOutsideObj);
-//				if (_handleEvents({ obj: clickOutsideObj, evType: 'clickoutside', eve: e, otherObj: el })) {	// clickoutside sends the target also.
 				if (_handleEvents({ obj: clickOutsideObj, evType: 'clickoutside', eve: e, component: compDetails.component, compDoc: compDetails.compDoc, varScope: compDetails.varScope, evScope: compDetails.evScope, otherObj: el })) {	// clickoutside sends the target also.
 					if (!clickOutsideSels[cid][1]) {
 						// This is a blocking click outside, so cancel any further actions.
