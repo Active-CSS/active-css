5106a26ca5c339e01b2ec4edeaf607dbab15c639
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-05-12 18:25:00 +0100

    fix to typo

--- dist/v-2-5-1/full-source/prod.js
+++ dist/v-2-5-1/full-source/prod.js
@@ -7803,7 +7803,7 @@ const _getAttrOrProp = (el, attr, getProp, ind=null, func='') => {
 	}
 	// Check for property.
 	ret = (ind) ? el.options[ind][attr] : el[attr];
-	if (ret || typeof ret == 'string') {
+	if (ret && typeof ret == 'string') {
 		let newRet = ret.replace(/\\/gm, '\\\\');
 		return (isRender) ? _escapeItem(newRet) : newRet;	// properties get escaped as if they are from attributes.
 	}
