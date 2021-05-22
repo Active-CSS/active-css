dc84298912d48a4aa86aa42ff012990992771bd3
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-05-22 13:54:43 +0100

    Fix to include ajax-pre-get erroring in the now general ajax error handling code.

--- dist/v-2-5-2/full-source/prod.js
+++ dist/v-2-5-2/full-source/prod.js
@@ -7320,7 +7320,7 @@ const _ajaxCallbackDisplay = (o) => {
 	}
 	if (!o.error && o.preGet) {
 		// Run the afterAjaxPreGet event.
-		_handleEvents({ obj: o.obj, evType: 'afterAjaxPreGet' + ((o.error) ? o.errorCode : ''), eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo });
+		_handleEvents({ obj: o.obj, evType: 'afterAjaxPreGet', eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo });
 	} else {
 		// Run the post event - success or failure.
 		_ajaxDisplay(o);
@@ -7328,16 +7328,16 @@ const _ajaxCallbackDisplay = (o) => {
 };
 
 const _ajaxCallbackErr = (str, resp, o) => {
+	o.error = true;
+	o.errorCode = resp || '';
+	/* Handle error code event */
+	let generalEvent = 'afterAjax' + ((o.preGet) ? 'PreGet' : '') + ((o.formSubmit) ? 'Form' + (o.formPreview ? 'Preview' : o.formSubmit ? 'Submit' : '') : '');
+	let commonObj = { obj: o.obj, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo };
+	if (o.errorCode) _handleEvents(commonObj);
+	commonObj.evType = generalEvent + 'Error';
+	/* Handle general error event */
+	_handleEvents(commonObj);
 	if (!o.preGet) {
-		o.error = true;
-		o.errorCode = resp || '';
-		/* Handle error code event */
-		let generalEvent = 'afterAjax' + ((o.formSubmit) ? 'Form' + (o.formPreview ? 'Preview' : o.formSubmit ? 'Submit' : '') : '');
-		let commonObj = { obj: o.obj, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo };
-		if (o.errorCode) _handleEvents(commonObj);
-		commonObj.evType = generalEvent + 'Error';
-		/* Handle general error event */
-		_handleEvents(commonObj);
 		if (debuggerActive || !setupEnded && typeof _debugOutput == 'function') {
 			_debugOutput('Ajax callback error debug: failed with error "' + resp + '".');
 		}
--- src/utils/file-handling/_ajaxCallbackErr.js
+++ src/utils/file-handling/_ajaxCallbackErr.js
@@ -1,14 +1,14 @@
 const _ajaxCallbackErr = (str, resp, o) => {
+	o.error = true;
+	o.errorCode = resp || '';
+	/* Handle error code event */
+	let generalEvent = 'afterAjax' + ((o.preGet) ? 'PreGet' : '') + ((o.formSubmit) ? 'Form' + (o.formPreview ? 'Preview' : o.formSubmit ? 'Submit' : '') : '');
+	let commonObj = { obj: o.obj, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo };
+	if (o.errorCode) _handleEvents(commonObj);
+	commonObj.evType = generalEvent + 'Error';
+	/* Handle general error event */
+	_handleEvents(commonObj);
 	if (!o.preGet) {
-		o.error = true;
-		o.errorCode = resp || '';
-		/* Handle error code event */
-		let generalEvent = 'afterAjax' + ((o.formSubmit) ? 'Form' + (o.formPreview ? 'Preview' : o.formSubmit ? 'Submit' : '') : '');
-		let commonObj = { obj: o.obj, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo, _taEvCo: o._taEvCo };
-		if (o.errorCode) _handleEvents(commonObj);
-		commonObj.evType = generalEvent + 'Error';
-		/* Handle general error event */
-		_handleEvents(commonObj);
 		if (debuggerActive || !setupEnded && typeof _debugOutput == 'function') {
 			_debugOutput('Ajax callback error debug: failed with error "' + resp + '".');
 		}
