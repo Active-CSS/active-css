3f6ca1dd2393a63c0680e97bbcca4eacf756e155
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-06-08 15:57:55 +0100

    fix to change div tag in router template to something else

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,8 @@
 	module.exports = function(grunt) {
 		grunt.initConfig({
 			// Custom variables.
-			realVersion: '2.6.0',
-			fileVersion: '2-6-0',
+			realVersion: '2.6.1',
+			fileVersion: '2-6-1',
 
 			pkg: grunt.file.readJSON('package.json'),
 
