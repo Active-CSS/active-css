53ac2ed8735aa2d5ebd44080df95ca8525e787f6
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-06-14 22:55:51 +0100

    slight adjustment to @for syntax
    Remove "=" and replace with "from". Remove the not-to-zero option as it will be superceded by the implementation of the @if statement in a later release.

--- dist/v-2-7-0/full-source/prod.js
+++ dist/v-2-7-0/full-source/prod.js
@@ -4379,35 +4379,30 @@ const _handleFor = (loopObj, scopePrefix) => {
 	let { currentLoop, varScope } = loopObj;
 	let existingLoopRef = (loopObj.loopRef) ? loopObj.loopRef : '';
 
-	// eg. @for n = 1 to 10 (defaults to increment of 1)
-	// eg. @for n = 1 to 10 step 2
-	// eg. @for n = 10 to 1 step -1
-	// eg. @for n = 10 to -10 step -1
-	// eg. @for n = -10 to 0 step 1
-	// eg. @for n = 1 to 10 step 0.5
-	// eg. @for n = {numVar} to {numVar2} step {numVar3}
+	// eg. @for n from 1 to 10 (defaults to increment of 1)
+	// eg. @for n from 1 to 10 step 2
+	// eg. @for n from 10 to 1 step -1
+	// eg. @for n from 10 to -10 step -1
+	// eg. @for n from -10 to 0 step 1
+	// eg. @for n from 1 to 10 step 0.5
+	// eg. @for n from {numVar} to {numVar2} step {numVar3}
 	// etc.
 	// It works with up to 5 decimal places. Not recommended for use with more than several thousand iterations for performance reasons.
 
 	// Get the positions of the "from", "to" and "step" parts of the string.
 	let statement = currentLoop;
-	let notToZero = false;
-	if (statement.indexOf(' not-to-zero') !== -1) {
-		notToZero = true;
-		statement = statement.replace('not-to-zero', '');
-	}
-	let fromPos = statement.indexOf('=');
+	let fromPos = statement.indexOf(' from ');
 	let toPos = statement.indexOf(' to ');
 	let stepPos = statement.indexOf(' step ');
 
 	if (fromPos === -1 || toPos === -1) {
-		console.log('Active CSS error: "=" and "to" must be used in the @for statement, "' + statement + '"');
+		console.log('Active CSS error: "from" and "to" must be used in the @for statement, "' + statement + '"');
 		return;
 	}
 
 	// Extract each part of the string that we need to run the statement and assign to appropriate variables.
 	let counterVar = statement.substr(5, fromPos - 5).trim();
-	let fromVar = statement.substr(fromPos + 1, toPos - fromPos - 1);
+	let fromVar = statement.substr(fromPos + 6, toPos - fromPos - 6);
 	let toVar, stepVar;
 
 	if (stepPos === -1) {
@@ -4434,8 +4429,8 @@ const _handleFor = (loopObj, scopePrefix) => {
 		return;
 	}
 
-	// If either "step" is set to zero, not-to-zero is set and "to" is zero, or there is a negative progression with no negative "step" value, skip loop.
-	if (stepVal == 0 || notToZero && toVal == 0 || fromVal > toVal && stepVal > 0) return;
+	// If either "step" is set to zero, or there is a negative progression with no negative "step" value, skip loop.
+	if (stepVal == 0 || fromVal > toVal && stepVal > 0) return;
 
 	// console.log('_handleFor, counterVar:', counterVar, 'fromVal:', fromVal, 'toVal:', toVal, 'stepVal:', stepVal);	// Handy - leave this here.
 
