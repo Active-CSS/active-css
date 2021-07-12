48a82211a66b374623cc1e0409c8b16aac673bda
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-07-12 17:16:49 +0100

    fix to $RANDn-n

--- dist/v-2-7-0/full-source/prod.js
+++ dist/v-2-7-0/full-source/prod.js
@@ -7558,7 +7558,9 @@ const _replaceJSExpression = (sel, realVal=false, quoteIfString=false, varScope=
 const _replaceRand = (str) => {
 	if (str.indexOf('{$RAND') !== -1) {
 		str = str.replace(/\{\$RAND((HEX)?(STR)?([\d]+)?(\-)?([\d]+)?)?\}/gm, function(_, __, isHex, isStr, num, hyph, endNum) {
-			return hyph ? Math.floor(Math.random() * (endNum - num + 1) + num) : _random( ((num) ? num : 8) , (isStr ? true : false) , (isHex ? true : false) );
+			if (num) num = parseInt(num);
+			if (endNum) endNum = parseInt(endNum);
+			return hyph ? (Math.floor(Math.random() * (endNum - num + 1)) + num) : _random( ((num) ? num : 8) , (isStr ? true : false) , (isHex ? true : false) );
 		});
 	}
 	return str;
