4e509f32dd8d7ba4e03a0e02cd7e595c6b93ea33
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2021-11-16 22:26:20 +0000

    scope parameter added to take-class

--- dist/v-2-9-0/activecss-core-dev-2-9-0.js
+++ dist/v-2-9-0/activecss-core-dev-2-9-0.js
@@ -1539,9 +1539,18 @@ _a.Style = o => {
 
 _a.TakeClass = o => {
 	if (!_isConnected(o.secSelObj)) return false;
-	// Take class away from any element that has it.
-	let cl = o.actVal.substr(1);
-	_eachRemoveClass(cl, cl, o.doc);
+	// Take class away from any element that has it, with an optional scope parameter.
+	let aV = o.actVal;
+	let pos = aV.indexOf('scope(');
+	let cl, scope = '';
+	if (pos !== -1) {
+		cl = aV.substr(1, pos - 1).trim();
+		scope = aV.substr(pos + 6, aV.length - pos - 7);
+		o.actVal = cl;	// this is mutating - bad for the extension when that arrives - change this after it works.
+	} else {
+		cl = aV.substr(1);
+	}
+	_eachRemoveClass(cl, cl, o.doc, scope);
 	_a.AddClass(o);
 };
 
@@ -10036,8 +10045,8 @@ const _doDebug = (typ, primSel) => {
 	}
 };
 
-const _eachRemoveClass = (inClass, classToRemove, doc) => {
-	doc.querySelectorAll('.' + inClass).forEach(function (obj, index) {
+const _eachRemoveClass = (inClass, classToRemove, doc, scope='') => {
+	doc.querySelectorAll(':scope ' + scope + ' .' + inClass + ((scope !== '') ? ',:scope ' + scope + '.' + inClass : '')).forEach(function (obj, index) {
 		if (!obj) return; // element is no longer there.
 		ActiveCSS._removeClassObj(obj, classToRemove);
 	});
--- src/commands/built-in/TakeClass.js
+++ src/commands/built-in/TakeClass.js
@@ -1,7 +1,16 @@
 _a.TakeClass = o => {
 	if (!_isConnected(o.secSelObj)) return false;
-	// Take class away from any element that has it.
-	let cl = o.actVal.substr(1);
-	_eachRemoveClass(cl, cl, o.doc);
+	// Take class away from any element that has it, with an optional scope parameter.
+	let aV = o.actVal;
+	let pos = aV.indexOf('scope(');
+	let cl, scope = '';
+	if (pos !== -1) {
+		cl = aV.substr(1, pos - 1).trim();
+		scope = aV.substr(pos + 6, aV.length - pos - 7);
+		o.actVal = cl;	// this is mutating - bad for the extension when that arrives - change this after it works.
+	} else {
+		cl = aV.substr(1);
+	}
+	_eachRemoveClass(cl, cl, o.doc, scope);
 	_a.AddClass(o);
 };
