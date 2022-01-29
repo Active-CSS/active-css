5584836642d66827aa44c6813b4f69bb764167f0
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2022-01-29 10:35:24 +0000

    update build after removing comments

--- dist/v-2-10-0/activecss-core-dev-2-10-0.js
+++ dist/v-2-10-0/activecss-core-dev-2-10-0.js
@@ -6095,12 +6095,6 @@ const _parseConfig = (str, inlineActiveID=null) => {
 	str = str.replace(/(?:[\s\;\{]?)break\;/g, 'break:1;');
 	str = str.replace(/(?:[\s\;\{]?)exit\;/g, 'exit:1;');
 	str = str.replace(/(?:[\s\;\{]?)exit\-target\;/g, 'exit\-target:1;');
-
-//	str = str.replace(/(?:[\{;])(?:[\s]*)continue\;/g, 'continue:1;');
-//	str = str.replace(/(?:[\{;])(?:[\s]*)break\;/g, 'break:1;');
-//	str = str.replace(/(?:[\{;])(?:[\s]*)exit\;/g, 'exit:1;');
-//	str = str.replace(/(?:[\{;])(?:[\s]*)exit\-target\;/g, 'exit\-target:1;');
-
 		
 	str = str.replace(/_ACSS_continue/g, 'continue;');
 	str = str.replace(/_ACSS_break/g, 'break;');
