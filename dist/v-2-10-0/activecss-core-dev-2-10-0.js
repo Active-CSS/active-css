ae54450fd8d3d8ddf5cec59587be786f84e622ec
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2022-04-17 11:45:21 +0100

    new toggle-attribute command

--- dist/v-2-10-0/activecss-core-dev-2-10-0.js
+++ dist/v-2-10-0/activecss-core-dev-2-10-0.js
@@ -1609,6 +1609,19 @@ _a.TakeClass = o => {
 	_a.AddClass({ secSelObj: o.secSelObj, actVal: theClass });
 };
 
+_a.ToggleAttribute = o => {
+	if (!_isConnected(o.secSelObj)) return false;
+	let arr = o.actVal.split(' ');
+	let attr = arr[0];
+	let force = arr[1] ? arr[1].trim() : null;
+	if (force) force = (force === 'true') ? true : (force === 'false') ? false : null;
+	if (force !== null) {
+		o.secSelObj.toggleAttribute(attr, force);
+	} else {
+		o.secSelObj.toggleAttribute(attr);
+	}
+};
+
 _a.ToggleClass = o => {
 	if (!_isConnected(o.secSelObj)) return false;
 	let str = o.actVal.replace(/\./g, '');
