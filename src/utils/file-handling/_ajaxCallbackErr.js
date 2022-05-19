ab0997e28cd15ce5f3821c1773e0d6a2e0405647
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2022-05-19 20:49:05 +0100

    Set up ajax failure events for components loading content

--- src/utils/file-handling/_ajaxCallbackErr.js
+++ src/utils/file-handling/_ajaxCallbackErr.js
@@ -28,12 +28,34 @@
 
 	// Handle error code event.
 	let generalEvent = 'afterAjax' + ((o.preGet) ? 'PreGet' : '') + ((o.formSubmit) ? 'Form' + (o.formPreview ? 'Preview' : o.formSubmit ? 'Submit' : '') : '');
-	let commonObj = { obj: o.obj, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo };
-	if (o.errorCode) _handleEvents(commonObj);
-	commonObj.evType = generalEvent + 'Error';
 
-	// Handle general error event.
-	_handleEvents(commonObj);
+	// This handles an ajax error from a rendered component importing a file.
+	let objToUse, isComponent;
+	if (!o.obj && o.renderObj && o.renderObj.renderO && o.renderObj.renderO.evObj) {
+		objToUse = o.renderObj.renderO.evObj.obj;
+		isComponent = true;
+		// The general failure event should only happen once per component. It is possible to have both html and css files failing, but we only want one event.
+		// When a component has failed, the attribute data-ajax-failure gets added (lower down). That is used to track whether to run further error events.
+		// If it isn't a component, we are not considered with any of that and just show the errors.
+	} else {
+		objToUse = o.obj;
+	}
+
+	if (!isComponent || !objToUse.hasAttribute('data-ajax-failure')) {
+		let commonObj = { obj: objToUse, evType: generalEvent + o.errorCode, eve: o.e, otherObj: o, varScope: o.varScope, evScope: o.evScope, compDoc: o.compDoc, component: o.component, _maEvCo: o._maEvCo };
+		if (o.errorCode) _handleEvents(commonObj);
+		commonObj.evType = generalEvent + 'Error';
+
+		// Handle general error event.
+		_handleEvents(commonObj);
+	}
+
+	if (isComponent) {
+		// The component is effectively rendered useless if there has been an ajax import failure, so we mark the element so the developer can remove it manually.
+		// We do this rather than remove it automatically because it puts the control into the developer's hands and they can see the failure.
+		objToUse.setAttribute('data-ajax-failure', resp);
+	}
+
 	if (!o.preGet) {
 		if (debuggerActive || !setupEnded && typeof _debugOutput == 'function') {
 			_debugOutput('Ajax callback error debug: failed with error "' + resp + '".');
