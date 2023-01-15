8f31cb48be0106474870ff79c6706eb08954bba9
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2023-01-15 16:37:13 +0000

    change intersection observer method to from ratio to isIntersecting to fix edge case

--- dist/v-2-12-0/activecss-core-dev-2-12-0.js
+++ dist/v-2-12-0/activecss-core-dev-2-12-0.js
@@ -2390,7 +2390,7 @@ const _handleClickOutside = (el, e) => {
 // Renders the now visible component when render-when-visible option is on component.
 const _handleCompIO = entries => {
 	entries.forEach(entry => {
-		if (entry.intersectionRatio > 0) {
+		if (entry.isIntersecting) {
 			let el = entry.target;
 			let props = el._acssCompIO;
 			el.removeAttribute('data-pending-visible');
--- src/core/runtime/_handleCompIO.js
+++ src/core/runtime/_handleCompIO.js
@@ -1,7 +1,7 @@
 // Renders the now visible component when render-when-visible option is on component.
 const _handleCompIO = entries => {
 	entries.forEach(entry => {
-		if (entry.intersectionRatio > 0) {
+		if (entry.isIntersecting) {
 			let el = entry.target;
 			let props = el._acssCompIO;
 			el.removeAttribute('data-pending-visible');
