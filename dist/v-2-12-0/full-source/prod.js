4977140256de5575302ab84ee28e68657d832b23
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2023-03-06 20:00:27 +0000

    remove comments

--- dist/v-2-12-0/full-source/prod.js
+++ dist/v-2-12-0/full-source/prod.js
@@ -201,8 +201,6 @@
 		mediaQueriesOrig = [],
 		mimicClones = [],
 		nonPassiveEvents = [],
-		observeEventsQueue = {},
-		observeEventsMid = {},
 		pageList = [],
 		pageWildcards = [],
 		pageWildReg = [],
@@ -2838,41 +2836,6 @@ const _handleObserveEvents = (mutations, dom, justCustomSelectors=false) => {
 	// This can get called a lot of times in the same event stack, and we only need to do once per criteria, so process a queue.
 	if (!dom) dom = document;          // Don't move this into parameter default.
  
-	// This can get called a lot of times, so process a queue and don't queue duplicate calls back into this function.
-	// _handleEvents, which is used in here, can generate more calls back into this function, which is necessary, but not if we already have the same
-	// thing already queued.
-	// The key to this is to remember that we do process this function if dom + justCustomSelectors is a unique entry for this queue.
-	// observeEventsQueue and observeEventsMid are objects so that states can be deleted cleanly when ended with minimal fuss.
-/*            comment out for the moment - seems to be a loop going on sometimes.
-	let ref, skipQueue;
-	if (dom.nodeType == 9) {
-		// This is the document.
-		ref = 'doc' + justCustomSelectors;
-	} else {
-		// This is a document fragment.
-		let domFirstChild = dom.firstChild;
-		if (domFirstChild) {
-			if (domFirstChild === Node.TEXT_NODE) return; // If the document only has a text node then it's not a valid document for ACSS. Skip observe events.
-			ref = (domFirstChild._acssActiveID) ? dom.firstChild._acssActiveID : _getActiveID(dom.firstChild).substr(3);
-		} else {
-			// It shouldn't really get in here, but if it does due to an empty component, just skip the queueing and run.
-			skipQueue = true;
-		}
-	}
- 
-	if (!skipQueue) {
-		if (observeEventsQueue[ref]) return;	       // Already queued to the end of the event stack - skip.
-		if (observeEventsMid[ref]) {
-			observeEventsQueue[ref] = true;
-			setTimeout(() => {
-				delete observeEventsQueue[ref];
-				_handleObserveEvents(mutations, dom, justCustomSelectors);
-			}, 0);
-		}
-		observeEventsMid[ref] = true;
-	}
-*/
-
 	// Handle cross-element observing for all observe events.
 	let evType = 'observe', i, primSel, compSelCheckPos, testSel, compDetails;
 	if (!selectors[evType]) return;
@@ -2902,8 +2865,6 @@ const _handleObserveEvents = (mutations, dom, justCustomSelectors=false) => {
 			});
 		}
 	}
- 
-//            if (!skipQueue) delete observeEventsMid[ref];
 };
 
 const _handleShadowSpecialEvents = shadowDOM => _handleObserveEvents(null, shadowDOM);
