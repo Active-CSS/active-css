959142de426c304c507b037453cbc13903b8a633
CEO River Zend Dynamics <42515015+bob2517@users.noreply.github.com>
2023-04-18 18:51:47 +0100

    Allow @if statements to parse variables that have a syntax of $var{@data-attr}

--- dist/v-2-12-1/activecss-core-dev-2-12-1.js
+++ dist/v-2-12-1/activecss-core-dev-2-12-1.js
@@ -5475,7 +5475,8 @@ const _runIf = (parsedStatement, originalStatement, ifObj) => {
 	// Substitute any variables dynamically so they have the correct values at the point of evaluation and not earlier.
 	let { obj, otherObj, varScope } = ifObj;
 
-	let strObj = _handleVars([ 'rand', 'expr', 'attrs', 'scoped' ],
+	// Attributes get evaluated first. This is so that we can evaluate things like $cheese{@data-num}, which is really useful.
+	let strObj = _handleVars([ 'attrs' ],
 		{
 			str: parsedStatement,
 			func: 'Var',
@@ -5483,6 +5484,19 @@ const _runIf = (parsedStatement, originalStatement, ifObj) => {
 			varScope
 		}
 	);
+
+	// Output the variables for real from the map.
+	let readyStatement = _resolveVars(strObj.str, strObj.ref);
+
+	strObj = _handleVars([ 'rand', 'expr', 'scoped' ],
+		{
+			str: readyStatement,
+			func: 'Var',
+			obj,
+			varScope
+		}
+	);
+
 	strObj = _handleVars([ 'strings' ],
 		{
 			str: strObj.str,
@@ -5502,14 +5516,14 @@ const _runIf = (parsedStatement, originalStatement, ifObj) => {
 	);
 
 	// Output the variables for real from the map.
-	let readyStatement = _resolveVars(strObj.str, strObj.ref);
+	readyStatement = _resolveVars(strObj.str, strObj.ref);
 
 	// Finally, remove any line breaks, otherwise things will barf when evaluated.
 	readyStatement = readyStatement.replace(/\r|\n/gm, '');
-	
+              
 	let res;
 	try {
-		res = Function('scopedProxy, ifObj, _runAtIfConds, escapeHTML, unEscapeHTML, getVar', '"use strict";return (' + readyStatement + ');')(scopedProxy, ifObj, _runAtIfConds, escapeHTML, unEscapeHTML, getVar);		// jshint ignore:line
+		res = Function('scopedProxy, ifObj, _runAtIfConds, escapeHTML, unEscapeHTML, getVar', '"use strict";return (' + readyStatement + ');')(scopedProxy, ifObj, _runAtIfConds, escapeHTML, unEscapeHTML, getVar);                                // jshint ignore:line
 	} catch (err) {
 		console.log('Active CSS error: Error in evaluating @if statement, "' + originalStatement + '", check syntax.');
 		console.log('Internal expression evaluated: ' + readyStatement, 'error:', err);
